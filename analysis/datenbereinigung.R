92281695da6fcad071885939df56c85ac0c86a48
marievreden <vredenmarie@gmail.com>
2022-09-12 14:28:35 +0200

    projekt 7 code weitergeschrieben

--- analysis/datenbereinigung.R
+++ analysis/datenbereinigung.R
@@ -1,26 +1,30 @@
-# 2.0 Setup -------------------------------------------------------------
-# 2.0.1
-# (Installiere und) lade die Pakete janitor und haven
+# 7.1 Setup -------------------------------------------------------------
+
+# 7.1.1 Projekt aufsetzen
+# * Setze die Ordnerstruktur für dieses Paket auf. Orientiere dich dabei an den
+#   Projekten 1 bis 6
+# * Lade den Datensatz aus OSF (https://osf.io/cwq6u/) in den richtigen Ordner
+
+# 7.1.2 Pakete laden
+# Lade die Pakete janitor und haven
 library(janitor)
 library(haven)
 
-# 2.1 Daten einlesen ------------------------------------------------------
+# 7.2 Daten einlesen ------------------------------------------------------
 
-# 2.1.0
-# * Lese den Datensatz data/motivation_data.sav ein
-# * Speichere den Datensatz in der Variable student_data
+# 7.2.1
+# * Lese den zuvor heruntergeladenen Datensatz ein und speichere ihn unter 
+#   einem sinnvollen Namen
 motivation_data <- read_sav("data/motivation_data.sav")
 
 
-# 2.2 Daten bereinigen ---------------------------------------------
+# 7.3 Daten bereinigen ---------------------------------------------
 
-# 2.2.0
+# 7.3.1
 # * Wandle die Variablennamen mit clean_names in snake case um
-# * Kodiere die Variable practice_type mit case_when um: 1 -> retrieval,
-#   2 -> restudy
-#   Speichere die umkodierte Variable unter dem gleichen Variablennamen
-# * Verbinde beide Bereinigungsschritte mit dem Pipe-Operator
-# * Speichere den bereinigten Datensatz in der Variable motivation_data_cleaned
+# * Kodiere die Variable practice_type mit case_when um und überschreibe sie:
+#   1 -> retrieval, 2 -> restudy
+# * Speichere den bereinigten Datensatz als neue Variable
 motivation_data_cleaned <- motivation_data %>% 
   clean_names(case = "snake") %>%
   mutate(
@@ -30,10 +34,10 @@ motivation_data_cleaned <- motivation_data %>%
     ))
 
 
-# 2.3 Datenexport ---------------------------------------------------------
+# 7.3 Datenexport ---------------------------------------------------------
 
-# 2.3.0
-# * Exportiere den Datensatz in den Ordner data/cleaned
+# 7.3.1
+# * Exportiere den Datensatz in den richtigen Ordner
 # * Speichere die Daten sowohl als csv-Datei als auch als sav-Datei
 write_csv(motivation_data_cleaned, "data/export/motivation_data_cleaned.csv")
 write_sav(motivation_data_cleaned, "data/export/motivation_data_cleaned.csv")
--- analysis/datenexploration.R
+++ analysis/datenexploration.R
@@ -1,42 +1,74 @@
+# 7.4 Datenexploration ---------------------------------------------------------
 # Beantworte folgende Fragen zum Datensatz. Berichte die Ergebnisse auch in 
 # ganzen Sätzen.
 
+# 7.4.1
 # * Wie viele Probanden waren jeweils in den beiden Versuchsbedingungen?
-# * Wie viele Fremdsprachen beherrschten die Probanden durchschnittlich?
-# * Wie viel Zeit verbrachten die Teilnehmenden der beiden Versuchsbedingungen
-#   jeweils mit der Powerpointpräsentation? Stelle das Ergebnis auch in einem
-#   Balkendiagramm dar.
-# * Unterscheiden sich die Gruppen in ihrem Interesse an Fremdsprachen? Stelle
-#   das Ergebnis auch in zwei Boxplots dar.
-# * Verbrachten diejenigen, die mehr Interesse an Schwedisch bzw. an
-#   Fremdsprachen haben, längere Zeit mit der Powerpointpräsentation? Stelle
-#   dies in einem Punktdiagramm dar
-# * Schnitten diejenigen, die mindestens drei Fremdsprachen beherrschen, besser
-#   im Abschlusstest ab, als diejenigen, die weniger als drei Fremdsprachen
-#   sprechen?
-# * Schnitten die Teilnehmenden aus der Retrieval-Gruppe besser im Abschlusstest
-#   ab als die Teilnehmenden aus der Restudy-Gruppe?
-# * Visualisiere für die Retrieval-Gruppe mit einem geeigneten geom, inwiefern
-#   sich ihre Leistung beim Abruf der Vokabeln über die drei Testzeitpunkte 
-#   verbessert hat.
-
 motivation_data_cleaned %>% 
   count(practice_type)
 
+# 7.4.2
+# * Wie viele Fremdsprachen beherrschten die Probanden durchschnittlich?
 mean(motivation_data_cleaned$number_languages, na.rm = TRUE)
 
+# 7.4.3
+# * Schnitten die Teilnehmenden aus der Retrieval-Gruppe besser im Abschlusstest
+#   ab als die Teilnehmenden aus der Restudy-Gruppe?
+motivation_data_cleaned %>% 
+  group_by(practice_type) %>% 
+  summarise(
+    mean_final = mean(final_test)
+  )
+
+# 7.4.4
+# * Wie viel Zeit verbrachten die Teilnehmenden der beiden Versuchsbedingungen
+#   jeweils mit der Powerpointpräsentation? Stelle das Ergebnis auch in einem
+#   Balkendiagramm dar und speichere die Visualisierung im richtigen Ordner.
 motivation_data_cleaned %>% 
   group_by(practice_type) %>% 
   summarise(
     mean_powerpoint = mean(time_free_choice_period)
+  ) %>% 
+ggplot(aes(x = practice_type, y = mean_powerpoint)) +
+  geom_col(width = 0.40) +
+  labs(
+    x = "condition",
+    y = "time in sec"
   )
 
+# 7.4.5
+# * Unterscheiden sich die Gruppen in ihrem Interesse an Fremdsprachen? Stelle
+#   das Ergebnis auch in zwei Boxplots dar.
 motivation_data_cleaned %>% 
   group_by(practice_type) %>% 
   summarise(
     mean_interest_lang = mean(interest_foreign_lang)
   )
 
+motivation_data_cleaned %>% 
+  ggplot(aes(x = practice_type, y = interest_foreign_lang)) +
+  geom_boxplot() +
+labs(
+  x = "condition",
+  y = "language interest"
+)
+
+# 7.4.6
+# * Visualisiere für die Retrieval-Gruppe mit einem geeigneten geom, inwiefern
+#   sich ihre Leistung beim Abruf der Vokabeln über die drei Testzeitpunkte 
+#   verbessert hat.
+motivation_data_cleaned %>% 
+  filter(practice_type == "retrieval") %>% 
+  summarise(
+    mean_cycle1 = mean(retrieval_practice_cycle1),
+    mean_cycle2 = mean(retrieval_practice_cycle2),
+    mean_final = mean(final_test)
+  )
+
+# 7.4.7
+# * Schnitten diejenigen, die mindestens drei Fremdsprachen beherrschen, besser
+#   im Abschlusstest ab, als diejenigen, die weniger als drei Fremdsprachen
+#   sprechen?
 motivation_data_cleaned %>%
   mutate(
     min_3_lang = case_when(
@@ -49,16 +81,7 @@ motivation_data_cleaned %>%
     mean_final= mean(final_test)
   )
 
-motivation_data_cleaned %>% 
-  group_by(practice_type) %>% 
-  summarise(
-    mean_final = mean(final_test)
-  )
-
-motivation_data_cleaned %>% 
-  filter(practice_type == "retrieval") %>% 
-  summarise(
-    mean_cycle1 = mean(retrieval_practice_cycle1),
-    mean_cycle2 = mean(retrieval_practice_cycle2),
-    mean_final = mean(final_test)
-  )
+# 7.4.8
+# * Verbrachten diejenigen, die mehr Interesse an Schwedisch bzw. an
+#   Fremdsprachen haben, längere Zeit mit der Powerpointpräsentation? Stelle
+#   dies in einem Punktdiagramm dar und speichere die Visualisierung ab
